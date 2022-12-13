ea9876b1a512891b4a8ae65cbd16f3d4251bfdb2
David <david.salvatella.gelpi@gmail.com>
2022-12-13 23:11:28 +0100

    button updated

--- frontend-test/src/components/Navbar.jsx
+++ frontend-test/src/components/Navbar.jsx
@@ -23,12 +23,7 @@ function Navbar(day_cnt, hour_cnt, minutes_cnt, raffleIsOpened) {
 
   return (
     <div>
-    <nav class="navbar" style={{backgroundColor: '#000000',
-      color: '#ffffff',
-      padding: '10px',
-      display: 'flex',
-      justifyContent: 'start'}}>
-    
+    <nav class="navbar" style={{backgroundColor: '#000000'}}>
       <a href="https://twitter.com/teamheretics">
       <img class="logo" style={{marginTop: '15px'}} alt="Archivo:Twitter-logo.svg" src="//upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/512px-Twitter-logo.svg.png" decoding="async" width="46" height="36" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/768px-Twitter-logo.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1024px-Twitter-logo.svg.png 2x" data-file-width="512" data-file-height="421" />
       </a>
@@ -38,11 +33,9 @@ function Navbar(day_cnt, hour_cnt, minutes_cnt, raffleIsOpened) {
       <a href="https://www.twitch.tv/team/teamheretics">
       <img class="logo" style={{marginTop: '9px'}} alt="File:Twitch logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c540.png" decoding="async" width="50" height="50" srcset="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Twitch_logo.svg/683px-Twitch_logo.svg.png?20150410204019 1.5x, https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Twitch_logo.svg/910px-Twitch_logo.svg.png?20150410204019 2x" data-file-width="455" data-file-height="281" />
       </a>
-      <div>
         <a href="https://teamheretics.com/">
-          <img class="logo" style={{marginTop: '9px', marginLeft: '630px'}} src= {img} alt="File:Heretics logo.png" width="150" height="50"/>
+          <img class="logo-heretics" style={{marginTop: '9px', marginLeft: '630px'}} src= {img} alt="File:Heretics logo.png" width="150" height="50"/>
         </a>
-      </div>
       <div>
       <span class="state" style={{ position: "relative", color: 'gold', fontSize: '36px', marginLeft: '400px', marginBottom: '50px'}}>Activo: {time}</span>
     </div>
