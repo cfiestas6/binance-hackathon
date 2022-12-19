76182061af0679878d2811bdf23b790ec2024432
Carlos <cfiestas6@gmail.com>
2022-12-19 23:02:36 +0100

    Meta tx

--- blockend/contracts/Reward.sol
+++ blockend/contracts/Reward.sol
@@ -5,12 +5,17 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 error Reward__ContractPaused();
 error Reward__NotWinner();
 error Reward__MaxSupplyReached();
 error Reward__AlreadyWhitelisted();
 error Reward__LimitReached();
+
+interface RewardInterface {
+    function mint(address winner) external payable;
+}
 
 contract Reward is ERC721Enumerable, Ownable {
     using Strings for uint256; 
@@ -37,18 +42,21 @@ contract Reward is ERC721Enumerable, Ownable {
         s_numAddressesWhitelisted += 1;
         whitelistedAddresses[msg.sender] = true;
     }
-    function mint() public payable {
+    function mint(address winner) external payable /*onlyOwner*/ {
         if (s_paused) {
             revert Reward__ContractPaused();
         }
-        if (!whitelistedAddresses[msg.sender]){
+        if (!whitelistedAddresses[winner]){
             revert Reward__NotWinner();
         }
         if (s_tokenIds >= s_maxWinners) {
             revert Reward__MaxSupplyReached();
         }
         s_tokenIds += 1;
-        _safeMint(msg.sender, s_tokenIds);
+        // Owner mints the NFT 
+        _safeMint(winner, s_tokenIds);
+        // Owner sends it to the winner
+        //safeTransferFrom(msg.sender, winner, s_tokenIds);
     } 
     function setPaused(bool val) public onlyOwner {
         s_paused = val;
@@ -59,7 +67,7 @@ contract Reward is ERC721Enumerable, Ownable {
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexisten token");
         string memory baseURI = _baseURI(); 
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, "1", ".json")) : "";
     }
     function withdraw() public onlyOwner {
         address _owner = owner();
@@ -70,4 +78,35 @@ contract Reward is ERC721Enumerable, Ownable {
 
     receive() external payable {}
     fallback() external payable {}
+}
+
+contract MintingMetaTx {
+
+    using ECDSA for bytes32;
+
+    // New mapping
+    mapping(bytes32 => bool) executed;
+
+    // Add the nonce parameter here
+    function mint(address sender, uint tokenId, address tokenContract, uint nonce, bytes memory signature) public {
+        // Pass ahead the nonce
+        bytes32 messageHash = getHash(sender, tokenId, tokenContract, nonce);
+        bytes32 signedMessageHash = messageHash.toEthSignedMessageHash();
+
+        // Require that this signature hasn't already been executed
+        require(!executed[signedMessageHash], "Already executed!");
+
+        address signer = signedMessageHash.recover(signature);
+
+        require(signer == sender, "Signature does not come from sender");
+
+        // Mark this signature as having been executed now
+        executed[signedMessageHash] = true;
+        RewardInterface(tokenContract).mint(sender);
+    }
+
+    // Add the nonce parameter here
+    function getHash(address sender, uint tokenId, address tokenContract, uint nonce) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(sender, tokenId, tokenContract, nonce));
+    }
 }
